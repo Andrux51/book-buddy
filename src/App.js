10cbb4b3baa67138ea252a11376559b719186a99
Andy Martin <andymartin51@gmail.com>
2018-04-14 22:07:31 -0600

    fix formatting issue

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
 import './App.css';
 import WelcomePage from './components/Welcome.page';
 import GetStartedPage from './components/GetStarted.page';
 
 export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <Link to="/">
-              <img src="http://placehold.it/80x80?text=logo" className="App-logo" alt="logo" />
-            </Link>
-            <h1 className="App-title">Welcome to BookBuddy</h1>
-          </header>
-          <div>
-            <Route
-              exact
-              path="/"
-              component={WelcomePage}
-            />
-            <Route
-              path="/get-started"
-              component={GetStartedPage}
-            />
-          </div>
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <Link to="/">
+                            <img
+                                src="http://placehold.it/80x80?text=logo"
+                                className="App-logo"
+                                alt="logo"
+                            />
+                        </Link>
+                        <h1 className="App-title">
+                            Welcome to BookBuddy
+                        </h1>
+                    </header>
+                    <div>
+                        <Route
+                            exact
+                            path="/"
+                            component={WelcomePage}
+                        />
+                        <Route
+                            path="/get-started"
+                            component={GetStartedPage}
+                        />
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
