412d6a5e6baba330900590ccfb87ec594d678476
Andy Martin <andymartin51@gmail.com>
2018-04-14 21:14:32 -0600

    cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-    Provider
-} from 'react-redux';
+import {Provider} from 'react-redux';
 import {MemoryRouter} from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import {mount} from 'enzyme';
